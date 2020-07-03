4288ab6a24ce9eec570398adc3c2e22ed8d02966
Aleksandr <markovkin88@gmail.com>
2020-07-03 21:56:56 +0300

    Initial commit

--- webpack.dev.js
+++ webpack.dev.js
@@ -9,6 +9,11 @@ module.exports = merge(common, {
     filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist')
   },
+  devServer: {
+    contentBase: path.join(__dirname, `dist`),
+    open: true,
+    port: 1336,
+  },
   plugins: [
     new HtmlWebpackPlugin({
       template: './src/template.html',
